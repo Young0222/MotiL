92a53e01b186f70403c79bc2f19e28207a470f1d
Anonymous <90235275+Young0222@users.noreply.github.com>
2024-12-11 13:33:43 +0800

    Delete DIMO_macromolecule directory

--- DIMO_macromolecule/finetune_motil.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# For the GO dataset
-python go.py --pretrain 'False' --gpu 6 --level 'cc' --batch-size 64 --ckpt-path './ckpt_finetune' --lr 1e-3  --wd 5e-4 --num-epochs 300 \
---base-width 32 --kernel-channels 24 --lr-milestone 300 400
-
-# For the EC dataset
-python ec.py --pretrain 'False' --gpu 6 --batch-size 24 --ckpt-path './ckpt_finetune'
